fa120c8da759374ff855cf947855929ed0330c0d
Umio Yasuno <53935716+Umio-Yasuno@users.noreply.github.com>
2022-05-05 10:32:45 +0900

    add examples/amdgpu_info.rs

---
+++ examples/amdgpu_info.rs
@@ -0,0 +1,46 @@
+use libdrm_amdgpu_sys::*;
+
+use std::fs::File;
+use std::os::unix::io::IntoRawFd;
+
+fn main() {
+    let v = File::open("/dev/dri/renderD128").unwrap();
+    let fd = v.into_raw_fd();
+
+    let amdgpu_dev = AMDGPU::device_initialize(fd).unwrap();
+
+    let gpu_info = AMDGPU::query_gpu_info(amdgpu_dev).unwrap();
+    println!("{gpu_info:?}");
+
+    let mark_name = AMDGPU::get_marketing_name(amdgpu_dev).unwrap();
+
+    println!();
+    println!("Marketing Name: [{mark_name}]");
+    println!("DeviceID.RevID: {:#0X}.{:#0X}", gpu_info.asic_id, gpu_info.pci_rev_id);
+
+    let family = AMDGPU::FAMILY_NAME::get(gpu_info.family_id);
+    let asic_name = AMDGPU::ASIC_NAME::get(family, gpu_info.chip_external_rev);
+    let chip_class = asic_name.chip_class();
+
+    println!();
+    println!("Family: {family}");
+    println!("ASIC Name: {asic_name}");
+    println!("Chip class: {chip_class}");
+
+    let vram_type = AMDGPU::VRAM_TYPE::get(gpu_info.vram_type);
+    let peak_bw = vram_type.peak_bw_gb(gpu_info.max_memory_clk, gpu_info.vram_bit_width);
+
+    println!();
+    println!("VRAM Type: {vram_type}");
+    println!("VRAM Bit Width: {}-bit", gpu_info.vram_bit_width);
+    println!("Peak Memory BW: {peak_bw} GB/s");
+
+    let info = AMDGPU::INFO::device_info(amdgpu_dev).unwrap();
+    // println!("{:?}", info);
+
+    unsafe {
+        println!();
+        AMDGPU::vbios_size(fd, amdgpu_dev).unwrap();
+        AMDGPU::vbios_info(fd, amdgpu_dev).unwrap();
+    }
+}
