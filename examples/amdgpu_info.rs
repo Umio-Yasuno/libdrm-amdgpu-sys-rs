712f77d8d044d351f06ba34013a9a65c7117b0d9
Umio Yasuno <53935716+Umio-Yasuno@users.noreply.github.com>
2022-05-19 01:58:51 +0900

    update examples/amdgpu_info.rs

--- examples/amdgpu_info.rs
+++ examples/amdgpu_info.rs
@@ -12,63 +12,150 @@ fn main() {
     let amdgpu_dev = AMDGPU::DEVICE_HANDLE::init(fd).unwrap();
 
     let gpu_info = amdgpu_dev.query_gpu_info().unwrap();
+    let _ext_info = amdgpu_dev.device_info().unwrap();
+
+    /*
     println!("{gpu_info:?}");
-
-    let mark_name = amdgpu_dev.get_marketing_name().unwrap();
-
     println!();
-    println!("Marketing Name: [{mark_name}]");
-    println!("DeviceID.RevID: {:#0X}.{:#0X}", gpu_info.asic_id, gpu_info.pci_rev_id);
+    println!("{ext_info:?}");
+    */
 
-    let family = AMDGPU::FAMILY_NAME::from_id(gpu_info.family_id);
-    let asic_name = AMDGPU::ASIC_NAME::get(family, gpu_info.chip_external_rev);
-    let chip_class = asic_name.chip_class();
+    {
+        let mark_name = amdgpu_dev.get_marketing_name().unwrap();
 
-    println!();
-    println!("Family: {family}");
-    println!("ASIC Name: {asic_name}");
-    println!("Chip class: {chip_class}");
+        println!();
+        println!("Marketing Name: [{mark_name}]");
+        println!("DeviceID.RevID: {:#0X}.{:#0X}", gpu_info.asic_id, gpu_info.pci_rev_id);
 
-    let vram_type = AMDGPU::VRAM_TYPE::from_type_id(gpu_info.vram_type);
-    let peak_bw = vram_type.peak_bw_gb(gpu_info.max_memory_clk, gpu_info.vram_bit_width);
+        let family = gpu_info.get_family_name();
+        let asic_name = gpu_info.get_asic_name();
+        let chip_class = gpu_info.get_chip_class();
 
-    println!();
-    println!("VRAM Type: {vram_type}");
-    println!("VRAM Bit Width: {}-bit", gpu_info.vram_bit_width);
-    println!("Peak Memory BW: {peak_bw} GB/s");
+        println!();
+        println!("Family: {family}");
+        println!("ASIC Name: {asic_name}");
+        println!("Chip class: {chip_class}");
 
-    let info = amdgpu_dev.device_info().unwrap();
-    // let info = AMDGPU::INFO::device_info(amdgpu_dev).unwrap();
-    // println!("{:?}", info);
+        let vram_type = gpu_info.get_vram_type();
+        let peak_bw = gpu_info.peak_memory_bw_gb();
 
-    use libdrm_amdgpu_sys::AMDGPU::HW_IP::*;
+        println!();
+        println!("VRAM Type: {vram_type}");
+        println!("VRAM Bit Width: {}-bit", gpu_info.vram_bit_width);
+        println!("Peak Memory BW: {peak_bw} GB/s");
+    }
 
-    let hw_ip = amdgpu_dev.query_hw_ip_info(HW_IP_TYPE::GFX, 0).unwrap();
-    println!("GFX: {hw_ip:?}");
+    {
+        use libdrm_amdgpu_sys::AMDGPU::HW_IP::*;
+
+        let ip_list = [
+            HW_IP_TYPE::GFX,
+            HW_IP_TYPE::COMPUTE,
+            HW_IP_TYPE::DMA,
+            HW_IP_TYPE::UVD,
+            HW_IP_TYPE::VCE,
+            HW_IP_TYPE::UVD_ENC,
+            HW_IP_TYPE::VCN_DEC,
+            HW_IP_TYPE::VCN_ENC,
+            HW_IP_TYPE::VCN_JPEG,
+        ];
+
+        println!();
+        for ip_type in &ip_list {
+            let ip_info = amdgpu_dev.query_hw_ip_info(*ip_type, 0).unwrap();
+
+            let (major, minor) = ip_info.version();
+            let queues = ip_info.num_queues();
+
+            if major == 0 || queues == 0 { continue; }
+
+            println!("{ip_type}: {major}.{minor}, queues: {queues}", );
+        }
+    }
     
-    use libdrm_amdgpu_sys::AMDGPU::FW_VERSION::*;
-    let fw_ver = amdgpu_dev.query_firmware_version(FW_TYPE::VCE, 0, 0).unwrap();
-    println!("VCE FW: {:X}", fw_ver.version);
+    {
+        use libdrm_amdgpu_sys::AMDGPU::FW_VERSION::*;
 
-    let bus_info = PCI::BUS_INFO::drm_get_device2(fd).unwrap();
-    println!();
-    println!("{:?}", bus_info);
-    println!("{:?}", bus_info.get_link_info(PCI::STATUS::Max));
+        let fw_list = [
+            FW_TYPE::VCE,
+            FW_TYPE::UVD,
+            FW_TYPE::GMC,
+            FW_TYPE::GFX_ME,
+            FW_TYPE::GFX_PFP,
+            FW_TYPE::GFX_CE,
+            FW_TYPE::GFX_RLC,
+            FW_TYPE::GFX_MEC,
+            FW_TYPE::SMC,
+            FW_TYPE::SDMA,
+            FW_TYPE::SOS,
+            FW_TYPE::ASD,
+            FW_TYPE::VCN,
+            FW_TYPE::GFX_RLC_RESTORE_LIST_CNTL,
+            FW_TYPE::GFX_RLC_RESTORE_LIST_GPM_MEM,
+            FW_TYPE::GFX_RLC_RESTORE_LIST_SRM_MEM,
+            FW_TYPE::DMCU,
+            FW_TYPE::TA,
+            FW_TYPE::DMCUB,
+            FW_TYPE::TOC,
+        ];
 
-    use libdrm_amdgpu_sys::AMDGPU::VIDEO_CAPS::*;
-    let dec_caps = amdgpu_dev.get_video_caps(CAP_TYPE::DECODE).unwrap();
-    let dec_mpeg4 = dec_caps.get_codec_info(CODEC::MPEG4).is_supported();
+        println!();
+        for fw_type in &fw_list {
+            let query = amdgpu_dev.query_firmware_version(*fw_type, 0, 0);
 
-    println!();
-    println!("MPEG4 Decode: {}", dec_mpeg4);
+            let fw_info = match query {
+                Ok(v) => v,
+                Err(_) => continue,
+            };
+
+            let (ver, ftr) = (fw_info.version, fw_info.feature);
+
+            if ver == 0 { continue; }
+
+            println!("{fw_type} FW ver: {ver}, feature: {ftr}");
+        }
+    }
+
+    {
+        use libdrm_amdgpu_sys::AMDGPU::VIDEO_CAPS::*;
+
+        let codec_list = [
+            CODEC::MPEG2,
+            CODEC::MPEG4,
+            CODEC::VC1,
+            CODEC::MPEG4_AVC,
+            CODEC::HEVC,
+            CODEC::JPEG,
+            CODEC::VP9,
+            CODEC::AV1,
+        ];
+
+        let dec = amdgpu_dev.get_video_caps(CAP_TYPE::DECODE).unwrap();
+        let enc = amdgpu_dev.get_video_caps(CAP_TYPE::ENCODE).unwrap();
+
+        println!();
+        for codec in &codec_list {
+            let dec_cap = dec.get_codec_info(*codec).is_supported();
+            let enc_cap = enc.get_codec_info(*codec).is_supported();
+
+            println!("{codec} decode: {dec_cap}, encode: {enc_cap}");
+        }
+    }
+
+    {
+        let bus_info = PCI::BUS_INFO::drm_get_device2(fd).unwrap();
+        println!();
+        println!("{:?}", bus_info);
+        println!("{:?}", bus_info.get_link_info(PCI::STATUS::Max));
+    }
 
     unsafe {
         use libdrm_amdgpu_sys::AMDGPU::VBIOS::*;
 
-        println!();
         let vbios = amdgpu_dev.vbios_info(fd).unwrap();
         let ver_str = null_control_to_space(vbios.vbios_ver_str.to_vec());
 
+        println!();
         // println!("{:?}", vbios);
         println!("name: {}", String::from_utf8(vbios.name.to_vec()).unwrap());
         println!("pn: {}", String::from_utf8(vbios.vbios_pn.to_vec()).unwrap());
