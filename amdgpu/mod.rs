39c0d893ffc1d7d963cbb80c457adb2b0428b58d
Umio Yasuno <coelacanth_dream@protonmail.com>
2024-03-23 15:46:49 +0900

    pp_table

--- amdgpu/mod.rs
+++ amdgpu/mod.rs
@@ -94,6 +94,7 @@ mod video_caps;
 mod hw_ip;
 mod fw_version;
 mod sensor_info;
+pub mod pp_table;
 
 pub mod VBIOS {
     pub use super::vbios::*;
---
+++ amdgpu/pp_table.rs
@@ -0,0 +1,8 @@
+pub use crate::bindings::ppt::{
+    smu_v11_0_0_ppt::{smu_11_0_powerplay_table, PPTable_t as PPTable_smu_11_0_t},
+    smu_v11_0_7_ppt::{smu_11_0_7_powerplay_table, PPTable_t as PPTable_smu_11_0_7_t},
+    smu_v13_0_0_ppt::{smu_13_0_0_powerplay_table, PPTable_t as PPTable_smu_13_0_0_t},
+    smu_v13_0_7_ppt::{smu_13_0_7_powerplay_table, PPTable_t as PPTable_smu_13_0_7_t},
+};
+
+
