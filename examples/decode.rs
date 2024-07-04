25494c89140ce136ff0658b725e6292b366dfe0c
Umio Yasuno <coelacanth_dream@protonmail.com>
2024-07-04 16:20:48 +0900

    update examples/decode

--- examples/decode.rs
+++ examples/decode.rs
@@ -1,12 +1,10 @@
 use libdrm_amdgpu_sys::*;
-use std::fs::File;
-use std::io;
+use AMDGPU::VBIOS::VbiosParser;
 
 fn main() {
-    let vbios_image = std::fs::read("/tmp/vbios.rom").unwrap();
-
-    use AMDGPU::VBIOS::VbiosParser;
-
+    let mut args = std::env::args().skip(1);
+    let path = args.next().unwrap();
+    let vbios_image = std::fs::read(path).unwrap();
     let vbios_parser = VbiosParser::new(vbios_image);
 
     if let Some(name) = vbios_parser.get_vbios_name() {
@@ -14,13 +12,17 @@ fn main() {
     }
 
     let rom_header = vbios_parser.get_atom_rom_header().unwrap();
-    println!("{rom_header:#?}");
+    println!("{rom_header:#X?}");
+
     let data_table = vbios_parser.get_atom_data_table(&rom_header).unwrap();
-    println!("{data_table:#?}");
+    println!("{data_table:#X?}");
+
     let firmware_info = vbios_parser.get_atom_firmware_info(&data_table).unwrap();
     println!("{firmware_info:#?}");
-    let ppt_bytes = vbios_parser.get_powerplay_table_bytes(&data_table).unwrap();
-    let ppt = AMDGPU::PPTable::decode_with_smu_version(&ppt_bytes, (13, 0, 0));
 
-    println!("{ppt:#?}");
+    if let Some(ppt_bytes) = vbios_parser.get_powerplay_table_bytes(&data_table) {
+        // for Navi31
+        let ppt = AMDGPU::PPTable::decode_with_smu_version(&ppt_bytes, (13, 0, 0));
+        println!("{ppt:#?}");
+    }
 }
