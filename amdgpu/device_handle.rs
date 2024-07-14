0b930a77c54182178374f1a378d5123ab8032ef6
Umio Yasuno <coelacanth_dream@protonmail.com>
2024-07-14 18:32:50 +0900

    Revert "pub DeviceHandle::deinit"
    This reverts commit be08b7feb09ef8b44bddc007a208e0a8bd149797.

--- amdgpu/device_handle.rs
+++ amdgpu/device_handle.rs
@@ -65,9 +65,8 @@ impl DeviceHandle {
         }
     }
 
-    /// This method can result in double-freeing of DeviceHandle.
-    pub unsafe fn deinit(&self) -> Result<i32, i32> {
-        let r = bindings::amdgpu_device_deinitialize(self.0);
+    fn deinit(&self) -> Result<i32, i32> {
+        let r = unsafe { bindings::amdgpu_device_deinitialize(self.0) };
 
         query_error!(r);
 
@@ -405,7 +404,7 @@ impl DeviceHandle {
 
 impl Drop for DeviceHandle {
     fn drop(&mut self) {
-        unsafe { self.deinit().unwrap(); }
+        self.deinit().unwrap();
     }
 }
 
