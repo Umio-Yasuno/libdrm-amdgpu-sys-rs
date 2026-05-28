8e2edcee2603b253eca0164939373601e7c84949
Umio Yasuno <coelacanth_dream@protonmail.com>
2026-05-28 14:36:30 +0900

    add get_drm_version_struct_with_fd

--- drm_version.rs
+++ drm_version.rs
@@ -1,7 +1,9 @@
 #[cfg(feature = "link_drm")]
 use crate::bindings;
+
 use crate::AMDGPU::DeviceHandle;
 use core::ffi::CStr;
+use std::os::fd::{AsFd, AsRawFd};
 
 #[derive(Debug, Clone)]
 pub struct drmVersion {
@@ -48,3 +50,39 @@ impl DeviceHandle {
         })
     }
 }
+
+pub fn get_drm_version_struct_with_fd(fd: impl AsFd) -> Result<drmVersion, i32> {
+    #[cfg(feature = "link_drm")]
+    let (get_func, free_func) = (bindings::drmGetVersion, bindings::drmFreeVersion);
+    #[cfg(feature = "dynamic_loading")]
+    let (get_func, free_func) = (self.libdrm.drmGetVersion, self.libdrm.drmFreeVersion);
+
+    let fd = fd.as_fd();
+    let drm_ver_ptr = unsafe { get_func(fd.as_raw_fd()) };
+
+    if drm_ver_ptr.is_null() {
+        return Err(-libc::EFAULT);
+    }
+
+    let ver = unsafe { core::ptr::read(drm_ver_ptr) };
+
+    let [name, date, desc] = [ver.name, ver.date, ver.desc].map(|v| {
+        if v.is_null() {
+            String::new()
+        } else {
+            unsafe { CStr::from_ptr(v).to_str().map(|s| s.to_string()).unwrap_or_default() }
+        }
+    });
+
+    unsafe { free_func(drm_ver_ptr) }
+
+    Ok(drmVersion {
+        version_major: ver.version_major,
+        version_minor: ver.version_minor,
+        version_patchlevel: ver.version_patchlevel,
+        name,
+        date,
+        desc,
+    })
+}
+
